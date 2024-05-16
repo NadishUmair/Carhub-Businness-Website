f7adc6eaf4485636d8d12bf53a2f5c2e44e0603d
Nadish Umair <nadishumair403@gmail.com>
2024-05-17 02:00:54 +0500

    save changes

--- src/app/CarList/page.jsx
+++ src/app/CarList/page.jsx
@@ -1,79 +1,112 @@
 "use client"; // Ensure this is at the very top of the file
 
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import { FilterCar } from '@/components/CarData';
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { FilterCar } from "@/components/CarData";
+import FilterForm from "@/components/FilterForms/page";
+
 const CarList = () => {
   const searchParams = useSearchParams();
   const [carData, setCarData] = useState(null);
- const [cars,setCars]=useState([]);
+  const [cars, setCars] = useState([]);
 
   useEffect(() => {
     const data = {
-      make: searchParams.get('make'),
-      model: searchParams.get('model'),
-      zip: searchParams.get('zip'),
-      distance: searchParams.get('distance'),
-      carType: searchParams.get('carType'),
+      make: searchParams.get("make"),
+      model: searchParams.get("model"),
+      zip: searchParams.get("zip"),
+      distance: searchParams.get("distance"),
+      carType: searchParams.get("carType"),
     };
     setCarData(data);
-    const FilteredCars = FilterCar.filter(car => {
+    const anyFilterPresent = Object.values(data).some((value) => value);
+
+    if (anyFilterPresent) {
+      const FilteredCars = FilterCar.filter((car) => {
+        return (
+          (data.make && car.make === data.make) ||
+          (data.model && car.model === data.model) ||
+          (data.zip && car.location.includes(data.zip)) ||
+          (data.distance && car.distance <= data.distance) ||
+          (data.carType && car.cartype === data.carType)
+        );
+      });
+      setCars(FilteredCars);
+    } else setCars(FilterCar);
+  }, [searchParams]);
+  const handleFilterSubmit = (data) => {
+    console.log(data);
+    const refineFiltered = FilterCar.filter((car) => {
       return (
         (data.make && car.make === data.make) ||
         (data.model && car.model === data.model) ||
         (data.zip && car.location.includes(data.zip)) ||
         (data.distance && car.distance <= data.distance) ||
-        (data.carType && car.carType === data.carType)
+        (data.carType && car.cartype === data.carType) ||
+        (data.touring && car.touring === data.touring) ||
+        (data.cylinders && car.touring === data.cylinders)
       );
     });
-    setCars(FilteredCars);
-  }, [searchParams]);
-
+    setCars(refineFiltered);
+  };
   if (!carData) return <div>Loading...</div>;
-console.log(FilterCar);
+  console.log(FilterCar);
   return (
-    <div>
-             <div className='h-[50vh] flex flex-col justify-center items-center'>
-          <div>
-          <h1 className='text-center'>  Car Hub</h1>
-          </div>
-             </div>
-   
-          <div className='flex flex-wrap  gap-4 justify-center'>
-
+    <>
     
-            {
-              cars.map((item,index)=>{
-                return(
-                      <div key={index} className=''>
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-           <img class="h-[50%]" src={item.image} alt="Sunset in the mountains"/>
-             <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">{item.name}</div>
-               <p class="text-gray-700 text-base">
-              {item.shortDesc}
-               </p>
-           </div>
-            <div class="px-6 pt-4 pb-2">
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Dealer Name:{item.dealerName}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Millege:{item.millege}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mr-2 mb-2 ">Price:{item.price}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Location:{item.location}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">PostDate:{item.postDate}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Zip/Postal:{item.zippostal}</span>
-             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Model:{item.model}</span>
-          
-                  </div>
-           </div>
-                        </div>
-                )
-              })
-            }
-          </div>
-
-  
-   
-    </div>
+      <div className="h-[50vh] flex flex-col justify-center items-center">
+        <div>
+          <h1 className="text-center"> Car Hub</h1>
+        </div>
+      </div>
+       <div className="md:flex">
+            <div className="md:w-[20%]">
+              <FilterForm onFilterSubmit={handleFilterSubmit}/>
+            </div>
+      <div className="md:w-[80%] flex flex-wrap  gap-4 justify-center">
+        {cars.map((item, index) => {
+          return (
+            <div key={index} className="md:w-[30%]">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  className="h-[50%]"
+                  src={item.image}
+                  alt="Sunset in the mountains"
+                />
+                <div className="px-2 ">
+                  <div className="font-bold text-xl mb-2">{item.name}</div>
+                  <p className="text-gray-700 text-base">{item.shortDesc}</p>
+                </div>
+                <div className="px-2 pt-4 pb-1">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Dealer Name:{item.dealerName}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Millege:{item.millege}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mr-2 mb-2 ">
+                    Price:{item.price}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Location:{item.location}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    PostDate:{item.postDate}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Zip/Postal:{item.zippostal}
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Model:{item.model}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      </div>
+      </>
   );
 };
 
